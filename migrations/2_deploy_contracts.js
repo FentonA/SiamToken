83980659dec22338472eeee835829828e1b9b6a9
FentonA <Fentonalf@gmail.com>
2021-07-31 17:14:23 -0400

    Updated total supply

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,8 @@ let SiamToken = artifacts.require("./SiamToken");
 let SiamTokenSale = artifacts.require("./SiamSale");
 
 module.exports = function(deployer){
-    deployer.deploy(SiamToken, 2150000).then(function(){
-        let tokenPrice = 12000000000000;
+    deployer.deploy(SiamToken, 2000015000000).then(function(){
+        let tokenPrice = 12000000000000; // price of one siam token in gwei
         return deployer.deploy(SiamTokenSale, SiamToken.address, tokenPrice)
     });
 };
